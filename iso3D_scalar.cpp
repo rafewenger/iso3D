efdc9da994641a9216c8473cda5ca4c22d0da131
Rephael Wenger <wenger@cse.ohio-state.edu>
2024-11-20 15:19:35 +0000

    Implemented option -interior_vertices.

--- iso3D_scalar.cpp
+++ iso3D_scalar.cpp
@@ -26,6 +26,12 @@ const char * default_output_filename = "out.nrrd";
 bool flag_add_scalar(false);
 SCALAR_TYPE addend(0);
 
+// Apply operation to interior vertices.
+bool flag_interior_vertices(true);
+
+// Apply operation to boundary vertices.
+bool flag_boundary_vertices(true);
+
 // Set to true, if some operation modifies the scalar grid.
 bool flag_modified(false);
 
@@ -33,8 +39,11 @@ bool flag_modified(false);
 void parse_command_line(int argc, char ** argv);
 void add_scalar(const SCALAR_TYPE addend,
                 SCALAR_GRID3D & scalar_grid);
-void write_scalar_grid
+void add_scalar_to_interior_vertices
+(const SCALAR_TYPE addend, SCALAR_GRID3D & scalar_grid);
+  void write_scalar_grid
 (const char * output_filename, const SCALAR_GRID3D & scalar_grid);
+
 
 int main(int argc, char ** argv)
 {
@@ -51,7 +60,11 @@ int main(int argc, char ** argv)
       { throw error; }
 
     if (flag_add_scalar) {
-      add_scalar(addend, scalar_grid);
+      if (flag_interior_vertices && flag_boundary_vertices)
+        { add_scalar(addend, scalar_grid); }
+      else if (flag_interior_vertices) {
+        add_scalar_to_interior_vertices(addend, scalar_grid);
+      }
     }
 
     write_scalar_grid(output_filename, scalar_grid);
@@ -91,21 +104,70 @@ void add_scalar(const SCALAR_TYPE addend, SCALAR_GRID3D & scalar_grid)
 }
 
 
+void add_scalar_to_interior_vertices
+(const SCALAR_TYPE addend, SCALAR_GRID3D & scalar_grid)
+{
+  VERTEX_INDEX_TYPE iv = 0;
+  for (int z = 1; z+1 < scalar_grid.AxisSize(2); z++) {
+    for (int y = 1; y+1 < scalar_grid.AxisSize(1); y++) {
+      iv = z*scalar_grid.AxisIncrement(2) +
+        y*scalar_grid.AxisIncrement(1) + 1;
+      for (int x = 1; x+1 < scalar_grid.AxisSize(0); x++) {
+        const SCALAR_TYPE s = scalar_grid.Scalar(iv);
+        scalar_grid.SetScalar(iv, s+addend);
+        iv++;
+      }
+    }
+  }
+
+  flag_modified = true;
+}
+
+
 // *******************************************************************
 // Miscellaneous functions
 // *******************************************************************
 
 void usage_msg(std::ostream & out)
 {
-  out << "Usage: iso3D_scalar [-add {s}] {input nrrd file} [{output nrrd file}]"
+  out << "Usage: iso3D_scalar [OPTIONS] {input nrrd file} [{output nrrd file}]"
       << endl;
 }
 
+void options_msg(std::ostream & out)
+{
+  out << "Options:" << endl;
+  out << "  [-add {s}] [-interior_vertices] [-boundary_vertices]" << endl;
+}
 
 void usage_error()
 {
   usage_msg(cerr);
+  options_msg(cerr);
+  
   exit(-1);
+}
+
+void help_msg()
+{
+  usage_msg(cout);
+  cout << endl;
+  cout << "iso3D_scalar - Process a scalar grid." << endl;
+  cout << "  Mainly an example of reading/writing nrrd files," << endl;
+  cout << "  and processing vertices/cubes in a scalar grid." << endl;
+  cout << endl;
+  cout << "Options:" << endl;
+  cout << "  -add {s}: Add scalar value {s} to each grid vertex." << endl;
+  cout << "  -interior_vertices: Apply operations only to grid interior vertices." << endl;
+  cout << "  -boundary_vertices: Apply operations only to grid boundary vertices." << endl;
+  cout << "  -all_vertices: Apply operations to all vertices." << endl;
+}
+
+
+void help()
+{
+  help_msg();
+  exit(0);
 }
 
 void parse_command_line(int argc, char ** argv)
@@ -121,6 +183,21 @@ void parse_command_line(int argc, char ** argv)
         get_arg_float(iarg, argc, argv, error);
       addend = SCALAR_TYPE(x);
       iarg++;
+    }
+    else if (s == "-interior_vertices") {
+      flag_interior_vertices = true;
+      flag_boundary_vertices = false;
+    }
+    else if (s == "-boundary_vertices") {
+      flag_interior_vertices = false;
+      flag_boundary_vertices = true;
+    }
+    else if (s == "-all_vertices") {
+      flag_interior_vertices = true;
+      flag_boundary_vertices = true;
+    }
+    else if (s == "-help") {
+      help();
     }
     else {
       cerr << "Usage error. Illegal parameter: " << s << endl;
